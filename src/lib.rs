cae1ae208b957f5d2a7428133328dfaebb1ee947
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-06-13 13:14:34 -0400

    Initial Commit

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+
+pub struct Entity<T> where T: core::any::Any, { 
+    data: T,
+}
+
+pub struct World<T: 'static> { 
+    components: HashMap<String,Vec<Entity<T>>>,
+}
+
+impl<T> World<T> { 
+    pub fn new()-> Self { 
+        Self {
+            components: HashMap::new(),        }
+    }
+
+    pub fn register_component(&mut self,name: &str) { 
+        self.components.insert(name.to_owned(), vec![]);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        let mut world = World::new();
+
+        world.register_component("location");
+
+        // Create a bird
+        let location:(f32,f32) = (0.0,0.0);
+    }
+}
