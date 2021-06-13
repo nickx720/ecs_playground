9ae4e0162f3b3921a008eb17ce44e3a0ee13240b
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-06-13 13:38:32 -0400

    Dynamic dispatch

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
 
-pub trait Component {}
 
+#[derive(Default)]
 pub struct World { 
-    components: HashMap<String,Vec<Box<dyn Component>>>,
+    components: HashMap<String,Vec<Option<Box<dyn core::any::Any>>>>,
 }
 
 impl World { 
@@ -14,6 +14,25 @@ impl World {
 
     pub fn register_component(&mut self,name: &str) { 
         self.components.insert(name.to_owned(), vec![]);
+    }
+
+    /// When creating a new entity we will want to add a new element to the component vectors in
+    /// preparation for that entity.
+    pub fn create_entity(&mut self) -> &mut Self {
+        self.components.iter_mut().for_each(|(_name,components)|{ 
+            components.push(None);
+        });
+        self
+    }
+
+    /// With is always called for create entity, so we can be sure that the last element of t he
+    /// vector is what we are updating.
+    pub fn with(&mut self, name: &str, data: Box<dyn core::any::Any>) -> &mut Self {
+        if let Some(components) = self.components.get_mut(name){ 
+            let index = components.len()-1;
+            components[index] = Some(data);
+        }        
+        self
     }
 }
 
@@ -28,5 +47,7 @@ mod tests {
 
         // Create a bird
         let location:(f32,f32) = (0.0,0.0);
+
+        world.create_entity().with("location",Box::new(location));
     }
 }
