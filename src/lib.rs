7f378e2b13427b51162f1d31b6c86782965ea3cf
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-06-13 15:31:45 -0400

    Lecture 1 over

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,21 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt::Debug, ops::Add};
 
+
+pub trait Component <T>
+where 
+    T:(core::any::Any)+ Debug{ 
+        fn get(&self) -> Box<T>;
+        fn get_mut(&mut self)-> Box<&mut T>;
+    }
+
+pub type ComponentData<T> = Vec<Option<Box<dyn Component<T>>>>;
 
 #[derive(Default)]
-pub struct World { 
-    components: HashMap<String,Vec<Option<Box<dyn core::any::Any>>>>,
+pub struct World<T> { 
+    components: HashMap<String,ComponentData<T>>,
 }
 
-impl World { 
+impl<T> World<T> { 
     pub fn new()-> Self { 
         Self {
             components: HashMap::new(),        }
@@ -27,27 +36,75 @@ impl World {
 
     /// With is always called for create entity, so we can be sure that the last element of t he
     /// vector is what we are updating.
-    pub fn with(&mut self, name: &str, data: Box<dyn core::any::Any>) -> &mut Self {
+    pub fn with(&mut self, name: &str, data: Box<dyn Component<T>>) -> &mut Self {
         if let Some(components) = self.components.get_mut(name){ 
             let index = components.len()-1;
             components[index] = Some(data);
         }        
         self
     }
+
+    pub fn get(&self,name:&str)-> Option<&Vec<Option<Box<dyn Component<T>>>>> { 
+        self.components.get(name)
+    }
+
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[derive(Debug)]
+    struct Point(f32,f32);
+
+    impl Component<(f32,f32)> for Point { 
+        fn get(&self)->Box<(f32,f32)> { 
+            Box::new((self.0,self.1))
+        }
+
+        fn get_mut(&mut self)-> Box<&mut (f32,f32 )> { 
+            Box::new(&mut (self.0,self.1))
+        }
+
+    }
+
+struct UpdateLocationsSystemg;
+
+impl<T: Debug> UpdateLocationsSystem { 
+    pub fn update_locations_system(&mut self, world:&mut World<T>) { 
+
+    }
+}
+
+  //  fn update_locations_system<'a, T:Debug>(world:&mut World<T>) { 
+  //      let mut locations = world.get("location").unwrap();
+  //      let mut velocities = world.get("velocity").unwrap();
+
+  //      let length = locations.len();
+
+  //      locations.iter_mut().enumerate().for_each(|(index,location)|{
+  //          if let Some(location) = location { 
+  //              let mut location = location.get_mut();
+  //              dbg!(location);
+  //          }
+  //      })
+  //  }
+
     #[test]
-    fn it_works() {
+    fn creating_the_world() {
         let mut world = World::new();
 
         world.register_component("location");
+        world.register_component("velocity");        
 
-        // Create a bird
-        let location:(f32,f32) = (0.0,0.0);
+        world
+            .create_entity()
+            .with("location",Box::new(Point(0.0,0.0)))
+            .with("velcity", Box::new(Point(15.0,15.0)));
 
-        world.create_entity().with("location",Box::new(location));
+        let locations = world.get("location").unwrap();
+        if let Some(location) = &locations[0]{
+            assert_eq!(location.get(),Box::new((0.0,0.0)))
+        }
     }
 }
